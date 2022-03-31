2eaefe94c1b103ebe28c55b774f24a958628c9f7
itzguled <itzguled55337@gmail.com>
2022-03-31 13:37:11 -0500

    comment route and file structure

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const { User, Comment, Post } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.get("/", (req, res) => {
+  Comment.findAll()
+    .then((CommentData) => res.json(CommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", withAuth, (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.session.user_id,
+    post_id: req.body.post_id,
+  })
+    .then((CommentData) => res.json(CommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.delete("/:id", withAuth, (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((CommentData) => {
+      if (!CommentData) {
+        res.status(404).json({ message: "No comment found with this id!" });
+        return;
+      }
+      res.json(CommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
